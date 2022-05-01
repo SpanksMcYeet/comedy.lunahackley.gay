a878b4c1bfbbb38b7198eec5023d5f1b566c7ded
Damocles <53099805+SpanksMcYeet@users.noreply.github.com>
2022-05-01 13:41:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-(async () => {
-  const characters = await fetch('./charList.json')
-  const CHAR = await characters.json()
-  document.getElementById('generateBypass').addEventListener('click', () => {
-    document.getElementById('showBypass').innerHTML = ''
-    let desiredBypass = document.getElementById('inputBypass').value
-    desiredBypass.split('')
-    for (let letter of desiredBypass) {
-      let desiredChars = CHAR[letter.toUpperCase()]
-      let desiredCharacter = desiredChars[Math.floor(Math.random() * desiredChars.length)]
-      if (desiredChars === null)
-        console.log(`Unable to find bypassable character for character ${letter}`)
-      document.getElementById('showBypass').innerHTML += desiredCharacter//String.fromCodePoint(parseInt(desiredCharacter, 16))
-    }
-  })
-})()
+import Unicode from './charList.js'
+
+document.getElementById('generateBypass').addEventListener('click', () => {
+  document.getElementById('showBypass').innerHTML = ''
+  let desiredBypass = document.getElementById('inputBypass').value
+  desiredBypass.split('')
+  for (let letter of desiredBypass) {
+    let desiredChars = Unicode[letter.toUpperCase()]
+    let desiredCharacter = desiredChars[Math.floor(Math.random() * desiredChars.length)]
+    if (desiredChars === null)
+      console.log(`Unable to find bypassable character for character ${letter}`)
+    document.getElementById('showBypass').innerHTML += desiredCharacter//String.fromCodePoint(parseInt(desiredCharacter, 16))
+  }
+})
