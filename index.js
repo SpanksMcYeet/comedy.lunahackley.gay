82c7ad778656b05626471f0b9b7a73c3fb557df9
Damocles <53099805+SpanksMcYeet@users.noreply.github.com>
2022-05-01 12:16:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
       let desiredCharacter = desiredChars[Math.floor(Math.random() * desiredChars.length)]
       if (desiredChars === null)
         console.log(`Unable to find bypassable character for character ${letter}`)
-      document.getElementById('showBypass').innerHTML += String.fromCodePoint(parseInt(desiredCharacter, 16))
+      document.getElementById('showBypass').innerHTML += desiredCharacter//String.fromCodePoint(parseInt(desiredCharacter, 16))
     }
   })
 })()
